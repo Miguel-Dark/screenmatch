03f5521c1fb185993d4db5c04564b1c3b881a663
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-01-31 05:50:46 -0600

    feat: refactor architecture with Principal class and user input interaction

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.principal.Principal;
 import com.aluracursos.screenmatch.service.ConsumoApi;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
@@ -22,34 +23,54 @@ public class ScreenmatchApplication implements CommandLineRunner {
     @Override
     // MÉTODO RUN: Punto de entrada para ejecutar lógica de consola en Spring Boot
     public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.muestraElMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // INSTANCIA: Crea el objeto para conectar con la API
-        var consumoApi = new ConsumoApi();
+        //var consumoApi = new ConsumoApi();
         // VARIABLE (String): Almacena la respuesta cruda (JSON) de la petición
-        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=32eabe1a");
+        //var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=32eabe1a");
         //var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-        System.out.println(json);
+        //System.out.println(json);
         // INSTANCIA: Crea el objeto conversor basado en nuestra clase de servicio
         //Instancia que convierte datos
-        ConvierteDatos conversor = new ConvierteDatos();
+        //ConvierteDatos conversor = new ConvierteDatos();
 
         // VARIABLE (Objeto): Almacena el resultado final de la conversión a DatosSerie
-        var datos = conversor.obtenerDatos(json, DatosSerie.class);
-        System.out.println(datos);
+        //var datos = conversor.obtenerDatos(json, DatosSerie.class);
+        //System.out.println(datos);
 
         // 1. Solicitamos los datos crudos (JSON) a la API usando una URL específica con parámetros de temporada y episodio.
-        json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=32eabe1a");
         // 2. Usamos el conversor genérico para transformar ese JSON en un objeto Java de tipo 'DatosEpisodio'.
         // Aquí sucede la magia de Jackson: mapea los campos de la API a nuestro Record.
-        DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+        //DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
         // 3. Imprimimos el objeto en consola. Gracias a que usamos un 'Record', Java nos da un toString() automático y bonito.
-        System.out.println(episodios);
+        //System.out.println(episodios);
 
-        List<DatosTemporadas> temporadas = new ArrayList<>();
-        for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
-            json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=32eabe1a");
-            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-            temporadas.add(datosTemporadas);
-        }
-        temporadas.forEach(System.out::println);
+
     }
 }
